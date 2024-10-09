48f5f74759ec95921bb2943450bdeff7d1fef8ca
Inês Martins <inesfmartins@ua.pt>
2024-10-09 15:13:14 +0100

    tentativa 2001

--- tdw-aula-p3/jest.config.js
+++ tdw-aula-p3/jest.config.js
@@ -1,22 +1,37 @@
 // jest.config.js
 
-module.exports = {
-  collectCoverage: true, // Ativa a coleta de cobertura
 
+module.exports = {
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
-  },
-
-  transform: {
-    "^.+\\.jsx?$": "babel-jest", // Adiciona suporte para JSX
-  },
-  testEnvironment: "jest-environment-jsdom", // Necessário para testes de componentes React
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+      statements: 80,
+    },
   },
 };
+
+
+// module.exports = {
+//   collectCoverage: true, // Ativa a coleta de cobertura
+
+//   coverageThreshold: {
+//     global: {
+//       branches: 80,
+//       functions: 80,
+//       lines: 80,
+//       statements: 80
+//     }
+//   },
+
+//   transform: {
+//     "^.+\\.jsx?$": "babel-jest", // Adiciona suporte para JSX
+//   },
+//   testEnvironment: "jest-environment-jsdom", // Necessário para testes de componentes React
+//   moduleNameMapper: {
+//     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+//   },
+// };
