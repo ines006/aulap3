68a694cdfc7bbf28734a29e6485e913da0a042b7
Inês Martins <inesfmartins@ua.pt>
2024-10-09 14:16:49 +0100

    jest coverage

--- tdw-aula-p3/jest.config.js
+++ tdw-aula-p3/jest.config.js
@@ -1,5 +1,15 @@
 // jest.config.js
+
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  
   transform: {
     "^.+\\.jsx?$": "babel-jest", // Adiciona suporte para JSX
   },
