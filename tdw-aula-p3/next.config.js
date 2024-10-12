0e593c3158aea95259062617dff841eef51e8d85
Inês Martins <inesfmartins@ua.pt>
2024-10-12 18:16:40 +0100

    preparacao p/ build

---
+++ tdw-aula-p3/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Habilita o modo estrito do React
+    output: 'export', // Isso habilita a exportação estática
+    swcMinify: true, // Ativa a minificação usando SWC para melhor performance
+    images: {
+        unoptimized: true    },
+  };
+  
+  module.exports = nextConfig;
+
+  
